72d656630fbca9a6ae3f8db0a3128df9949f7679
faisal <faisalmunadhilramzy.com>
2024-08-12 14:41:17 +0700

    menyelesaikan dynamic routing

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import RegisterPage from './pages/register';
 import ErrorPage from './pages/404.jsx';
 import ProductsPage from './pages/products.jsx';
 import ProfilePage from './pages/profile.jsx';
+import DetailProductPage from './pages/detailProduct.jsx';
 
 const router = createBrowserRouter([
   {
@@ -23,13 +24,17 @@ const router = createBrowserRouter([
     element: <RegisterPage />,
   },
   {
-    path: '/product',
+    path: '/products',
     element: <ProductsPage />,
   },
   {
     path: '/profile',
     element: <ProfilePage />,
   },
+  {
+    path: "/product/:id",
+    element: <DetailProductPage />
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
